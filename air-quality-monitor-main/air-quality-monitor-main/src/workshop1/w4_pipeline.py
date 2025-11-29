a15d6e10bb69fa1df6840d9537435526ba58806f
Kaliddamon <cristian.sanchez3@udea.edu.co>
2025-11-29 17:20:01 -0500

    Refactor imports in w4_pipeline.py

--- air-quality-monitor-main/air-quality-monitor-main/src/workshop1/w4_pipeline.py
+++ air-quality-monitor-main/air-quality-monitor-main/src/workshop1/w4_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-import workshop4
+from workshop4 import testMapReduce
 
 #mainpipelineforw4
 def run_w4_from_records(records):
@@ -16,4 +16,5 @@ def run_w4_from_records(records):
 
     chart_data = testMapReduce(records, worker_configs)
     return chart_data
+
 
