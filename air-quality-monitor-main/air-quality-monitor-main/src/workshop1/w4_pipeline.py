50ff72e65c16007a58120afb187a330677a000c6
Kaliddamon <cristian.sanchez3@udea.edu.co>
2025-11-30 17:43:09 -0500

    Update worker_configs to include an additional parameter

--- air-quality-monitor-main/air-quality-monitor-main/src/workshop1/w4_pipeline.py
+++ air-quality-monitor-main/air-quality-monitor-main/src/workshop1/w4_pipeline.py
@@ -7,15 +7,17 @@ from workshop4.mapreduce_parallel import testMapReduce
 def run_w4_from_records(records):
 
     worker_configs = [
-        (1, 1),
-        (2, 2),
-        (3, 2),
-        (4, 2),
-        (6, 2),
+        (1, 1, 1),
+        (2, 2, 2),
+        (3, 2, 2),
+        (4, 2, 2),
+        (6, 2, 2),
     ]
 
-    chart_data = testMapReduce(records, worker_configs)
-    return chart_data
+    w4_result = testMapReduce(records, worker_configs)
+    
+    return w4_result
+
 
 
 
