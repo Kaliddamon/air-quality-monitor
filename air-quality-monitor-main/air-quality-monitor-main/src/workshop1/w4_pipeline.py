56bba11d4544e5b41e46dc366df6de8e82d65867
Kaliddamon <cristian.sanchez3@udea.edu.co>
2025-12-01 17:03:01 -0500

    Update w4_pipeline.py

--- air-quality-monitor-main/air-quality-monitor-main/src/workshop1/w4_pipeline.py
+++ air-quality-monitor-main/air-quality-monitor-main/src/workshop1/w4_pipeline.py
@@ -4,19 +4,12 @@ import pandas as pd
 from workshop4.mapreduce_parallel import testMapReduce
 
 #mainpipelineforw4
-def run_w4_from_records(records):
-
-    worker_configs = [
-        (1, 1, 1),
-        (2, 2, 2),
-        (3, 2, 2),
-        (4, 2, 2),
-        (6, 2, 2),
-    ]
+def run_w4_from_records(records, worker_configs):
 
     w4_result = testMapReduce(records, worker_configs)
     
     return w4_result
+
 
 
 
