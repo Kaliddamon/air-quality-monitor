1de8391176f31c372921bccfcb112801abd69ae8
Kaliddamon <cristian.sanchez3@udea.edu.co>
2025-12-03 17:08:49 -0500

    Update geographic_knn.py

--- air-quality-monitor-main/air-quality-monitor-main/src/workshop6/geographic_knn.py
+++ air-quality-monitor-main/air-quality-monitor-main/src/workshop6/geographic_knn.py
@@ -1,21 +1,14 @@
 from workshop6.utils import *
 
-def knn_geographic(sensors, query_id=None, k=5, weight_type_penalty=200.0, weight_source=50.0, weight_coord=1.0):
+def knn_geographic(sensors, query, k=5, weight_type_penalty=200.0, weight_source=50.0, weight_coord=1.0):
     """
     Distancia compuesta:
       - penalty grande si geographicType distinto (para priorizar mismo tipo)
       - distancia euclidiana entre coordenadas (proximidad)
       - diferencia codificada de pollutionSource
     """
-    if query_id is None:
-        raise ValueError("Necesito query_id para knn_geographic")
-    query = None
-    for s in sensors:
-        if s["_id"] == query_id:
-            query = s
-            break
     if query is None:
-        raise ValueError("query_id no encontrado")
+        raise ValueError("Necesito query para knn_geographic")
 
     q_type = query["geographicType"]
     q_coords = query["coords"]
@@ -33,5 +26,6 @@ def knn_geographic(sensors, query_id=None, k=5, weight_type_penalty=200.0, weigh
         results.append((score, s))
     results.sort(key=lambda x: x[0])
     return results[:k]
+
 
 
